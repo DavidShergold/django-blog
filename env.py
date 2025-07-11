0d3bc8aa4b2980fc39e0df7c36f7915ba8d7dfcc
David <shergoldd24@gmail.com>
2025-07-11 15:04:25 +0100

    further updates

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_GIea2jq8VpKM@ep-polished-meadow-a2xejl0g.eu-central-1.aws.neon.tech/sepia_scorn_slash_809805")
